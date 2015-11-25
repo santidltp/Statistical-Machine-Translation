a28a9615f59484667c1094f5221ab038fbefce4d
Santiago <sdelatorrepi@umass.edu>
2015-11-24 23:43:25 -0500

    updated tokeniazer

--- SMT/tokenizer.py
+++ SMT/tokenizer.py
@@ -1,34 +1,42 @@
 __author__ = 'santiago'
 
 class tokenizer:
-    def __init__(self, file1, file2):
+
+
+    def tokenizer(self, file1):
         try:
             english_input = open(file1, 'r').read()
             # foreing_input = open(file2,'r').read()
             # print(english_input)
 
         except:
-            print("one of the files does not exist!")
-        pass
-    def tokenizer(self):
+            print("The files does not exist!")
 
         my_string = "This is nothing but a pure test. I mean, (just a test)."
-        broken = my_string.split(" ")
+        lines = english_input.split('\n')
+        words = [x.split(" ") for x in lines ]
+
+        # words = lines.split(" ")
         newString = ""
         pstPunct = [',','.','!',')','%','\'',"\"",':',';',']','?']
-        prePunct = ['¡','(','#','\'','\"','[','¿','@']
+        prePunct = ['¡','(','#','\'','\"','[','¿','@','$']
+        for line in lines:
+            ex = line.split(" ")
+            for word in ex:
 
-        for word in broken:
-            if any(x in word for x in pstPunct):
-                if ')' in word and '.' in word or ']' in word and '.' in word:
-                    newString += word[:-2] + " " + word[len(word)-2:len(word)-1] + " " +word[len(word)-1:]
+                if any(x in word for x in pstPunct):
+                    if ')' in word and '.' in word or ']' in word and '.' in word:
+                        newString += word[:-2] + " " + word[len(word)-2:len(word)-1] + " " + word[len(word)-1:]
 
+                    else:
+                        newString += word[:-1] + " " + word[len(word)-1:] + " "
+
+                elif any(x in word for x in prePunct):
+                    newString += word[0] + " " + word[1:] + " "
                 else:
-                    newString += word[:-1] + " " + word[len(word)-1:] + " "
-            elif any(x in word for x in prePunct):
-                newString += word[0] + " " + word[1:] + " "
-            else:
-                newString += word + " "
+                    newString += word + " "
+                # if '\n' in word:
+            newString+='\n'
 
 
         return self.truecasing(newString)
@@ -37,6 +45,5 @@ class tokenizer:
         return str.lower()
 
 if __name__ == '__main__':
-    n = tokenizer("en.txt","es.txt")
-    # n.tokenizer()
-    print(n.tokenizer())
+    n = tokenizer()
+    print(n.tokenizer("en.txt"))

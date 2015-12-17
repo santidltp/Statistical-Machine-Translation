fef964763b24d4a44afc54e2e625c3775ffe245b
Santiago <sdelatorrepi@umass.edu>
2015-12-17 14:12:58 -0500

    fixed small error

--- aligner/output.py
+++ aligner/output.py
@@ -18,7 +18,7 @@ class HTMLOutput:
         self.counter = 1
         self.mxFreq = None
         self.outFile = outputFile
-        outputFile.writer(
+        outputFile.write(
             '''<?xml version="1.0" encoding="%s"?>
                 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
                 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
